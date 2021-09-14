33e1de4c80fbe90541dfe4a3ee11f04ce52ccb7b
Drew <kevinfunderburkbusiness@gmail.com>
2021-09-14 14:46:19 -0500

    Updated DamageOnCollisionBehaviour

--- Gameplay/DamageOnCollisionBehaviour.cs
+++ Gameplay/DamageOnCollisionBehaviour.cs
@@ -1,31 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 /// <summary>
 /// Damage an object on collision
 /// 
 /// Depends on IDamageable
 /// </summary>
-public class DamageTargetOnCollisionBehaviour : MonoBehaviour
+public class DamageOnCollisionBehaviour : MonoBehaviour
 {
+    private enum DamageResult
+    {
+        NOTHING,
+        DESTROY,
+        DISABLE
+    }
+
     [Tooltip("How much damage to deal to the collided object")]
     [SerializeField] private float _damage = 1;
 
     [Tooltip("What layers this object should check when looking to deal damage")]
     [SerializeField] private LayerMask _damageMask = ~0;
 
+    [Space]
+    [Tooltip("What should happen to this object after it deals damage?")]
+    [SerializeField] private DamageResult _damageResult = DamageResult.NOTHING;
+
+    [Space]
+    public UnityEvent OnDamage;
+
     private void OnCollisionEnter(Collision collision)
     {
-        // If the other GameObject's layer is in _whatToDamage
-        if (((1 << collision.gameObject.layer) & _damageMask) != 0)
-        {
-            // Get a reference to the other object's HealthBehaviour
-            IDamageable otherHealth = collision.gameObject.GetComponent<IDamageable>();
+        // If the other GameObject's layer is not in _whatToDamage, return
+        if (((1 << collision.gameObject.layer) & _damageMask) == 0)
+            return;
 
-            // If it has a HealthBehaviour, damage it
-            if (otherHealth)
-                otherHealth.TakeDamage(_damage);
+        // If it can be damaged, damage it
+        if (collision.gameObject.TryGetComponent(out IDamageable other))
+        {
+            other.TakeDamage(_damage);
+            OnDamage.Invoke();
+
+            switch (_damageResult)
+            {
+                case DamageResult.NOTHING:
+                    break;
+                case DamageResult.DESTROY:
+                    Destroy(gameObject);
+                    break;
+                case DamageResult.DISABLE:
+                    gameObject.SetActive(false);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
