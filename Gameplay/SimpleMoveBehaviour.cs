e9a3d21a09d7cc120b0da0bd0488754b226d425a
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2022-03-27 19:11:53 -0500

    Added UtilityButton unity package for ease of installation

---
+++ Gameplay/SimpleMoveBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMoveBehaviour : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movementPerSecond;
+    [SerializeField] private bool _worldSpace = false;
+
+    private void Update()
+    {
+        if (_worldSpace)
+        {
+            transform.position += _movementPerSecond * Time.deltaTime;
+        }
+        else
+        {
+            Vector3 right = transform.right * _movementPerSecond.x;
+            Vector3 up = transform.up * _movementPerSecond.y;
+            Vector3 forward = transform.forward * _movementPerSecond.z;
+            Vector3 movement = right + up + forward;
+
+            transform.position += movement * Time.deltaTime;
+        }
+    }
+}
---
+++ Tools/History.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Queue with maximum limit. Automatically Dequeue's old items
+/// </summary>
+public class History<T> : Queue<T>
+{
+    private int _limit;
+
+    public int Limit
+    {
+        get => _limit;
+        set
+        {
+            // TODO: Expand or shrink Queue to be the exact right size
+            //  Potentially deep copy
+            //  Look into copying a larger queue into a smaller
+            if (value >= 0)
+            {
+                while (Count > value)
+                    Dequeue();
+
+                if (value > _limit)
+                    TrimExcess();
+
+                _limit = value;
+            }
+        }
+    }
+
+    public History(int limit) : base(limit)
+    {
+        _limit = limit;
+    }
+
+    public new void Enqueue(T item)
+    {
+        base.Enqueue(item);
+
+        while (Count > Limit)
+            Dequeue();
+    }
+}
