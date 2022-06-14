715269760b293534b46dd8b71725139ee850065a
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2022-06-14 10:58:51 -0500

    Cleaned up ObjectContainerSO and added tooltip

--- ScriptableObjects/ObjectContainerSO.cs
+++ ScriptableObjects/ObjectContainerSO.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Object Container", menuName = "ScriptableObjects/ObjectContainer")]
 public class ObjectContainerSO : ScriptableObject
 {
+    [Tooltip("List of objects held by this ObjectContainer")]
     [SerializeField] private GameObject[] _objects;
 
+    [Tooltip("List of objects held by this ObjectContainer")]
     public GameObject[] Objects { get => _objects; private set => _objects = value; }
 
-    public GameObject GetRandomObject()
-    {
-        int index = Random.Range(0, Objects.Length);
-
-        return Objects[index];
-    }
+    /// <summary>
+    /// Retrieve a random object from this container
+    /// </summary>
+    public GameObject GetRandomObject() => Objects[Random.Range(0, Objects.Length)];
 }
