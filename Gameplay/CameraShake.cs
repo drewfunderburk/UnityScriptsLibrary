efdac22d95c1adbf554cbde3031a79aedd4bad17
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2022-06-14 17:01:24 -0500

    Added amplitude and frequency controls to CameraShake

--- Gameplay/CameraShake.cs
+++ Gameplay/CameraShake.cs
@@ -22,13 +22,27 @@ public class CameraShake : MonoBehaviour
 
     [Tooltip("These settings are usually fine as default, but they're here if you'd like manual control over the noise")]
     [SerializeField] private NoiseSettings _noiseSettings;
+
     [Space]
     [Tooltip("Maximum distance the camera will translate on each axis at maximum trauma")]
     [SerializeField] private Vector3 _maximumTranslationalOffset = new Vector3(0.3f, 0.3f, 0.0f);
+
     [Tooltip("Maximum distance the camera will rotate on each axis at maximum trauma")]
     [SerializeField] private Vector3 _maximumRotationalOffset = new Vector3(10.0f, 10.0f, 10.0f);
+
+    [Space]
+    [Tooltip("How fast should the camera shake")]
+    [SerializeField, Range(0.01f, 3)] private float _frequency = 1;
+
+    [Tooltip("How hard should the camera shake")]
+    [SerializeField, Range(0.01f, 3)] private float _amplitude = 1;
+
     [Tooltip("Higher values will require the trauma to be higher before any intense shake is performed")]
     [SerializeField] private float _traumaExponent = 2.0f;
+
+    [Tooltip("Should trauma decay at all")]
+    [SerializeField] private bool _traumaShouldDecay = true;
+
     [Tooltip("How quickly should trauma decay from a certain value")]
     [SerializeField] private AnimationCurve _traumaDecayCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
@@ -36,6 +50,11 @@ public class CameraShake : MonoBehaviour
     private Vector2 _noiseScrollX;
     private Vector2 _noiseScrollY;
     private Vector2 _noiseScrollZ;
+
+    public float Trauma { get => _trauma; set => _trauma = value; }
+    public bool TraumaShouldDecay { get => _traumaShouldDecay; set => _traumaShouldDecay = value; }
+    public float Amplitude { get => _amplitude; set => _amplitude = value; }
+    public float Frequency { get => _frequency; set => _frequency = value; }
 
     private void Start()
     {
@@ -50,13 +69,13 @@ public class CameraShake : MonoBehaviour
     /// <param name="trauma"></param>
     public void AddTrauma(float trauma)
     {
-        _trauma = Mathf.Clamp01(_trauma + trauma);
+        Trauma = Mathf.Clamp01(Trauma + trauma);
     }
 
     private void Update()
     {
         // No need to process if there is no trauma
-        if (_trauma <= 0)
+        if (Trauma <= 0)
             return;
 
         // Apply translational shake
@@ -66,9 +85,9 @@ public class CameraShake : MonoBehaviour
             Vector3 noiseSample = SampleNoise();
             
             // Calculate positions
-            float translationX = _maximumTranslationalOffset.x * Mathf.Pow(_trauma, _traumaExponent) * noiseSample.x;
-            float translationY = _maximumTranslationalOffset.y* Mathf.Pow(_trauma, _traumaExponent) * noiseSample.y;
-            float translationZ = _maximumTranslationalOffset.z * Mathf.Pow(_trauma, _traumaExponent) * noiseSample.z;
+            float translationX = _maximumTranslationalOffset.x * Amplitude * Mathf.Pow(Trauma, _traumaExponent) * noiseSample.x;
+            float translationY = _maximumTranslationalOffset.y * Amplitude * Mathf.Pow(Trauma, _traumaExponent) * noiseSample.y;
+            float translationZ = _maximumTranslationalOffset.z * Amplitude * Mathf.Pow(Trauma, _traumaExponent) * noiseSample.z; 
 
             // Apply translation
             transform.localPosition = new Vector3(translationX, translationY, translationZ); ;
@@ -81,15 +100,16 @@ public class CameraShake : MonoBehaviour
             Vector3 noiseSample = SampleNoise();
 
             // Calculate positions
-            Quaternion rotationX = Quaternion.AngleAxis(_maximumRotationalOffset.x * Mathf.Pow(_trauma, _traumaExponent) * noiseSample.x, Vector3.right);
-            Quaternion rotationY = Quaternion.AngleAxis(_maximumRotationalOffset.y * Mathf.Pow(_trauma, _traumaExponent) * noiseSample.y, Vector3.up);
-            Quaternion rotationZ = Quaternion.AngleAxis(_maximumRotationalOffset.z * Mathf.Pow(_trauma, _traumaExponent) * noiseSample.z, Vector3.forward);
+            Quaternion rotationX = Quaternion.AngleAxis(_maximumRotationalOffset.x * Amplitude * Mathf.Pow(Trauma, _traumaExponent) * noiseSample.x, Vector3.right);
+            Quaternion rotationY = Quaternion.AngleAxis(_maximumRotationalOffset.y * Amplitude * Mathf.Pow(Trauma, _traumaExponent) * noiseSample.y, Vector3.up);
+            Quaternion rotationZ = Quaternion.AngleAxis(_maximumRotationalOffset.z * Amplitude * Mathf.Pow(Trauma, _traumaExponent) * noiseSample.z, Vector3.forward);
             
             transform.localRotation = rotationX * rotationY * rotationZ;
         }
 
         // Trauma decay
-        _trauma = Mathf.Max(0, _trauma - _traumaDecayCurve.Evaluate(_trauma) * Time.deltaTime);
+        if (TraumaShouldDecay)
+            Trauma = Mathf.Max(0, Trauma - _traumaDecayCurve.Evaluate(Trauma) * Time.deltaTime);
     }
 
     private Vector3 SampleNoise()
@@ -100,9 +120,9 @@ public class CameraShake : MonoBehaviour
         float noiseSampleZ = Mathf.PerlinNoise(_noiseScrollZ.x, _noiseScrollZ.y) * 2 - 1;
 
         // Scroll noise
-        _noiseScrollX += _noiseSettings.XAxisNoiseScrollSpeed * Time.deltaTime;
-        _noiseScrollY += _noiseSettings.YAxisNoiseScrollSpeed * Time.deltaTime;
-        _noiseScrollZ += _noiseSettings.ZAxisNoiseScrollSpeed * Time.deltaTime;
+        _noiseScrollX += _noiseSettings.XAxisNoiseScrollSpeed * Frequency * Mathf.Pow(Trauma, _traumaExponent) * Time.deltaTime;
+        _noiseScrollY += _noiseSettings.YAxisNoiseScrollSpeed * Frequency * Mathf.Pow(Trauma, _traumaExponent) * Time.deltaTime;
+        _noiseScrollZ += _noiseSettings.ZAxisNoiseScrollSpeed * Frequency * Mathf.Pow(Trauma, _traumaExponent) * Time.deltaTime;
 
         return new Vector3(noiseSampleX, noiseSampleY, noiseSampleZ);
     }
@@ -117,7 +137,7 @@ public class CameraShakeEditor : Editor
         string text = "This camera shake script uses a trauma system to give both rotational and translational shake to an object based on Perlin Noise." +
             "\n\n" +
             "Place directly on the object you want shaken. Object will be reset to its local origin when no trauma is applied. " +
-            "Child the object to an empty game object and affect it with any other scripts instead.";
+            "Child the object to an empty game object and affect that object with any other scripts instead.";
         EditorGUILayout.HelpBox(text, MessageType.Info);
 
         base.OnInspectorGUI();
